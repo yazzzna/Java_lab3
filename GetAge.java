dc120ba8e39bda333698c8f29a5e198ca9b190d2
yazzzna <145989666+yazzzna@users.noreply.github.com>
2024-11-24 00:10:49 +0300

    Update GetAge.java

--- GetAge.java
+++ GetAge.java
@@ -1,22 +1,27 @@
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-public class GetAge {
+class GetAge {
     public void FindAge(String birthday) {
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate data_now = LocalDate.now();
-        LocalDate data_birth = LocalDate.parse(birthday, formatter);
-        Period period = Period.between(data_birth, data_now);
 
-        int years = period.getYears();
-        if (years % 10 == 1 && years % 100 != 11) {
-            System.out.println(years + " год");
-        } else if (years % 10 >= 2 && years % 10 <= 4 && (years % 100 < 10 || years % 100 >= 20)) {
-            System.out.println(years + " года");
-        } else {
-            System.out.println(years + " лет");
+        try {
+            LocalDate data_birth = LocalDate.parse(birthday, formatter);
+            Period period = Period.between(data_birth, data_now);
+
+            int years = period.getYears();
+            if (years % 10 == 1 && years % 100 != 11) {
+                System.out.println(years + " год");
+            } else if (years % 10 >= 2 && years % 10 <= 4 && (years % 100 < 10 || years % 100 >= 20)) {
+                System.out.println(years + " года");
+            } else {
+                System.out.println(years + " лет");
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Ошибка: неверный формат даты");
         }
     }
 }
